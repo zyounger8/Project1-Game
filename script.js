1401557c303af0ab8ae903014f0fbc19b3a00352
Zee Louise <zeelouise@zees-imac.myfiosgateway.com>
2022-09-20 09:52:00 -0400

    test two

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
-
 //DOM - Items to Grab
-const myCanvas = document.querySelector('canvas').getContext('2d')
+const myCanvas = document.querySelector("canvas").getContext("2d");
 // const plyBtn = document.getElementById('#playButton')
 
 // Creating the Player - class
 
 class Player {
-    constructor(){
-        this.position = {
-            x: 200,
-            y: 200
-        }
-        this.velocity = {
-            x: 0,
-            y: 0
-        }
+  constructor() {
+    this.position = {
+      x: 200,
+      y: 200,
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
 
-const image = new Image()
-image.src = './images/player.png'
-
-        this.image = image
-            this.width = 100
-            this.height = 100
-    }
-    draw(){
-        // myCanvas.fillStyle = 'red'
-        // myCanvas.fillRect(this.position.x, this.position.y, this.width, this.height)
-        myCanvas.drawImage(this.image, this.position.x, this.position.y)
-    }
+    const image = new Image();
+    image.src = "./assets/player.png";
+    this.image = image;
+    this.width = 100;
+    this.height = 100;
+  }
+  draw() {
+    // myCanvas.fillStyle = 'red'
+    // myCanvas.fillRect(this.position.x, this.position.y, this.width, this.height)
+    myCanvas.drawImage(this.image, this.position.x, this.position.y);
+  }
 }
 
-const player = new Player()
-player.draw()
+const player = new Player();
+player.draw();
 
-function animate(){
-    requestAnimationFrame(animate)
-    player.draw()
+function animate() {
+  requestAnimationFrame(animate);
+  player.draw();
 }
-animate()
+animate();
